364afa5ea51b74a1cacae3acbe855ec4326e6866
booardman <60456931+booardman@users.noreply.github.com>
2020-09-08 15:38:46 -0700

    Redirect on login and logout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 
-function App(props) 
-  {const [isAuthenticated, userHasAuthenticated] = useState(false);
+function App(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
 
-function handleLogout() {
-  userHasAuthenticated(false);
-}
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+    
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    props.history.push("/login");
+  }
 
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -23,9 +46,9 @@ function handleLogout() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-          {isAuthenticated
-            ? <NavItem onClick={handleLogout}>Logout</NavItem>
-            : <>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
                 <LinkContainer to="/signup">
                   <NavItem>Signup</NavItem>
                 </LinkContainer>
@@ -33,7 +56,7 @@ function handleLogout() {
                   <NavItem>Login</NavItem>
                 </LinkContainer>
               </>
-          }
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -42,4 +65,4 @@ function handleLogout() {
   );
  }
 
-export default App;
+ export default withRouter(App);
