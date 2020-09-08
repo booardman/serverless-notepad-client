41e4a2f998d89f62cfc91f31c9cc93e4f780667c
booardman <60456931+booardman@users.noreply.github.com>
2020-09-08 15:03:51 -0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-va"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ofwkdeqz1c.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_9vwlO94Gn",
+    APP_CLIENT_ID: "4r21cs3cg2qvgj17kp6m8d8843",
+    IDENTITY_POOL_ID: "us-east-1:5ccf8222-0f11-4d9e-a041-284b62e535d9"
+  }
+};
