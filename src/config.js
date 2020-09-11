06f11c713ec86cf0154678586f302c43a8fd9b50
booardman <60456931+booardman@users.noreply.github.com>
2020-09-11 11:11:23 -0700

    Client testing successful, production ready

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_9vwlO94Gn",
     APP_CLIENT_ID: "4r21cs3cg2qvgj17kp6m8d8843",
-    IDENTITY_POOL_ID: "us-east-1:5ccf8222-0f11-4d9e-a041-284b62e535d9"
+    IDENTITY_POOL_ID: "us-east-1:adf1ccef-0f3c-4c6e-bd65-e009fd302deb"
   },
-  STRIPE_KEY: "51HMIOzDitwBAhzQBIupJ5PFwDPv73cBiMVhcdRPJP2gqQabVtPeHtr5PpG9tvMKLIM3DwM1tdzw567tCbaytr7Er00kxQtHxDj"
+  STRIPE_KEY: "pk_test_51HMIOzDitwBAhzQBIupJ5PFwDPv73cBiMVhcdRPJP2gqQabVtPeHtr5PpG9tvMKLIM3DwM1tdzw567tCbaytr7Er00kxQtHxDj"
 };
